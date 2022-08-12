14f6377e6d970287ca48dc0f956c4bee9fd854dd
Taylor White <whitetc2@gmail.com>
2022-08-12 14:46:40 -0400

    Change to WASM project

--- examples/BlazingAppleConsumer.Components/BlazingAppleConsumer.Components/Data/Base/ComparableModelBase.cs
+++ examples/BlazingAppleConsumer.Components/BlazingAppleConsumer.Components/Data/Base/ComparableModelBase.cs
@@ -1,59 +1,59 @@
 ﻿namespace BlazingAppleConsumer.Components.Data.Base
 {
-	/// <summary>Default implementation of <see cref="IComparable{T}" /></summary>
-	/// <typeparam name="TModel">The model type to compare against.</typeparam>
-	/// <typeparam name="TKey">The type of the key member.</typeparam>
-	public abstract class ComparableModelBase<TModel, TKey> : IComparable<ComparableModelBase<TModel, TKey>>
-		where TModel : ComparableModelBase<TModel, TKey>
-	{
-		public static bool operator !=(ComparableModelBase<TModel, TKey>? left, ComparableModelBase<TModel, TKey>? right)
-			=> !(left == right);
+    /// <summary>Default implementation of <see cref="IComparable{T}" /></summary>
+    /// <typeparam name="TModel">The model type to compare against.</typeparam>
+    /// <typeparam name="TKey">The type of the key member.</typeparam>
+    public abstract class ComparableModelBase<TModel, TKey> : IComparable<ComparableModelBase<TModel, TKey>>
+        where TModel : ComparableModelBase<TModel, TKey>
+    {
+        public static bool operator !=(ComparableModelBase<TModel, TKey>? left, ComparableModelBase<TModel, TKey>? right)
+            => !(left == right);
 
-		public static bool operator <(ComparableModelBase<TModel, TKey> left, ComparableModelBase<TModel, TKey> right)
-			=> left.CompareTo(right) < 0;
+        public static bool operator <(ComparableModelBase<TModel, TKey> left, ComparableModelBase<TModel, TKey> right)
+            => left.CompareTo(right) < 0;
 
-		public static bool operator <=(ComparableModelBase<TModel, TKey> left, ComparableModelBase<TModel, TKey> right)
-			=> left.CompareTo(right) <= 0;
+        public static bool operator <=(ComparableModelBase<TModel, TKey> left, ComparableModelBase<TModel, TKey> right)
+            => left.CompareTo(right) <= 0;
 
-		public static bool operator ==(ComparableModelBase<TModel, TKey>? left, ComparableModelBase<TModel, TKey>? right)
-					=> left?.Equals(right) ?? right is null;
+        public static bool operator ==(ComparableModelBase<TModel, TKey>? left, ComparableModelBase<TModel, TKey>? right)
+                    => left?.Equals(right) ?? right is null;
 
-		public static bool operator >(ComparableModelBase<TModel, TKey> left, ComparableModelBase<TModel, TKey> right)
-				=> left.CompareTo(right) > 0;
+        public static bool operator >(ComparableModelBase<TModel, TKey> left, ComparableModelBase<TModel, TKey> right)
+                => left.CompareTo(right) > 0;
 
-		public static bool operator >=(ComparableModelBase<TModel, TKey> left, ComparableModelBase<TModel, TKey> right)
-				=> left.CompareTo(right) >= 0;
+        public static bool operator >=(ComparableModelBase<TModel, TKey> left, ComparableModelBase<TModel, TKey> right)
+                => left.CompareTo(right) >= 0;
 
-		/// <inheritdoc cref="IComparable{T}" />
-		public int CompareTo(ComparableModelBase<TModel, TKey>? other)
-		{
-			if (other is null)
-				return 1;
-			else if (GetKeyValue() is Enum enumValue)
-				return enumValue.CompareTo(other.GetKeyValue());
-			else if (GetKeyValue() is IComparable<TKey> key)
-				return key.CompareTo(other.GetKeyValue());
-			else
-				throw new ArgumentOutOfRangeException("Unsupported value type provided");
-		}
+        /// <inheritdoc cref="IComparable{T}" />
+        public int CompareTo(ComparableModelBase<TModel, TKey>? other)
+        {
+            if (other is null)
+                return 1;
+            else if (GetKeyValue() is Enum enumValue)
+                return enumValue.CompareTo(other.GetKeyValue());
+            else if (GetKeyValue() is IComparable<TKey> key)
+                return key.CompareTo(other.GetKeyValue());
+            else
+                throw new ArgumentOutOfRangeException("Unsupported value type provided");
+        }
 
-		/// <inheritdoc />
-		public override bool Equals(object? obj)
-		{
-			if (ReferenceEquals(this, obj))
-				return true;
-			else if (obj is not ComparableModelBase<TModel, TKey> other)
-				return false;
-			else
-				return GetKeyValue().Equals(other.GetKeyValue());
-		}
+        /// <inheritdoc />
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return true;
+            else if (obj is not ComparableModelBase<TModel, TKey> other)
+                return false;
+            else
+                return GetKeyValue().Equals(other.GetKeyValue());
+        }
 
-		/// <inheritdoc />
-		public override int GetHashCode()
-			=> GetKeyValue().GetHashCode();
+        /// <inheritdoc />
+        public override int GetHashCode()
+            => GetKeyValue().GetHashCode();
 
-		/// <summary>Expression to provide the sort by property.</summary>
-		/// <returns>Returns the member to use in the compare to.</returns>
-		protected abstract TKey GetKeyValue();
-	}
+        /// <summary>Expression to provide the sort by property.</summary>
+        /// <returns>Returns the member to use in the compare to.</returns>
+        protected abstract TKey GetKeyValue();
+    }
 }
--- examples/BlazingAppleConsumer.Components/BlazingAppleConsumer.Components/Program.cs
+++ examples/BlazingAppleConsumer.Components/BlazingAppleConsumer.Components/Program.cs
@@ -1,35 +1,13 @@
-using BlazingAppleConsumer.Components.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using BlazingApple.FontAwesome.Services;
 using BlazingApple.Components.Configuration;
+using BlazingAppleConsumer.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddHttpClient();
 builder.Services.AddBlazingAppleComponents(builder.Configuration);
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+await builder.Build().RunAsync();
