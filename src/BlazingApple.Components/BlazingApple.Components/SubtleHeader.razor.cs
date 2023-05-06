c2f6d5b1a2a661718333d30f9862f2eafd5a593e
Taylor White <whitetc2@gmail.com>
2023-05-06 10:14:25 -0400

    Various improvements, paginator now allows going to first/last pages, see more toggle no longer broken, etc.

---
+++ src/BlazingApple.Components/BlazingApple.Components/SubtleHeader.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazingApple.Components;
+
+/// <summary></summary>
+public partial class SubtleHeader : ComponentBase
+{
+	/// <summary>The header details to show.</summary>
+	[Parameter, EditorRequired]
+	public string Name { get; set; } = null!;
+
+	/// <summary>Additional styles to apply to the badge.</summary>
+	[Parameter(CaptureUnmatchedValues = true)]
+	public IDictionary<string, object>? AdditionalAttributes { get; set; }
+
+	/// <summary>Child content to render</summary>
+	[Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
