640c8db51e1c58fe3ed5357f84420f37185f28d4
Taylor White <whitetc2@gmail.com>
2023-05-15 07:41:54 -0400

    Add new dropdown component

---
+++ src/BlazingApple.Components/BlazingApple.Components/HTMLElements/DropDown.razor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingApple.Components.HTMLElements;
+
+
+/// <summary>A drop down selection component</summary>
+/// <typeparam name="T">The values composed within the dropdown.</typeparam>
+public partial class DropDown<T> : ComponentBase
+{
+	/// <summary>Used to style the containing div.</summary>
+	[Parameter(CaptureUnmatchedValues = true)]
+	public IDictionary<string, object>? AdditionalAttributes { get; set; }
+
+	private bool _isOpen;
+
+	/// <summary><c>True</c> if the component should be disabled, <c>false</c> otherwise.</summary>
+	[Parameter]
+	public bool Disabled { get; set; }
+
+	/// <summary>Label/title for the component.</summary>
+	[Parameter]
+	public string? Label { get; set; }
+
+	/// <summary>Bootstrap button class, like <c>btn-outline-secondary</c> or <c>btn-primary btn-sm</c>.</summary>
+	[Parameter]
+	public string? ButtonClass { get; set; } = "btn-light";
+
+	/// <summary>Child content to render into the dropdown.</summary>
+	[Parameter]
+	public RenderFragment? ChildContent { get; set; }
+
+	/// <summary>The value of the dropdown.</summary>
+	[Parameter]
+	public T Value { get; set; } = default!;
+
+	/// <summary>Used to support bind-Value, invoked on the dropdown being changed.</summary>
+	[Parameter]
+	public EventCallback<T> ValueChanged { get; set; }
+
+	private List<DropDownItem<T>>? Items { get; set; }
+
+	/// <summary>To be called by children.</summary>
+	/// <param name="item">The component to add</param>
+	public void AddItem(DropDownItem<T> item)
+		=> Items!.Add(item);
+
+	/// <summary>
+	/// Called when a <see cref="DropDownItem{T}"/> is clicked.
+	/// </summary>
+	/// <param name="newVal">The new value.</param>
+	/// <returns>Async op.</returns>
+	public async Task OnValueSelect(T newVal)
+	{
+		Value = newVal;
+		_isOpen = false;
+
+		if (ValueChanged.HasDelegate)
+			await ValueChanged.InvokeAsync(newVal);
+
+		StateHasChanged();
+	}
+
+	/// <summary>Get data from the database to populate the input fields.</summary>
+	/// <returns>Nothing.</returns>
+	protected override async Task OnInitializedAsync()
+	{
+		await base.OnInitializedAsync();
+		Items ??= new List<DropDownItem<T>>();
+	}
+
+	private void ToggleOpen()
+		=> _isOpen = !_isOpen;
+}
---
+++ src/BlazingApple.Components/BlazingApple.Components/HTMLElements/DropDownItem.razor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazingApple.Components.HTMLElements;
+
+/// <summary>An item in <see cref="DropDown{T}"/>.</summary>
+/// <typeparam name="T">The type of item.</typeparam>
+public partial class DropDownItem<T> : ComponentBase
+{
+	/// <summary>Used to style the containing div.</summary>
+	[Parameter(CaptureUnmatchedValues = true)]
+	public IDictionary<string, object>? AdditionalAttributes { get; set; }
+
+	/// <summary>The value this item represents.</summary>
+	[Parameter]
+	public T Value { get; set; } = default!;
+
+	/// <summary>The content to display in the button</summary>
+	[Parameter, EditorRequired]
+	public RenderFragment? ChildContent { get; set; }
+
+	/// <summary>The parent filter collection.</summary>
+	[CascadingParameter]
+	protected DropDown<T> Parent { get; set; } = null!;
+
+	/// <inheritdoc />
+	protected override void OnInitialized()
+	{
+		base.OnInitialized();
+		Parent.AddItem(this);
+	}
+
+	/// <summary><c>True</c> if the component should be disabled, <c>false</c> otherwise.</summary>
+	[Parameter]
+	public bool Disabled { get; set; }
+
+	private async Task OnClick()
+		=> await Parent.OnValueSelect(Value);
+}
