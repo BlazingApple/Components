f67f0ab9207a5c9e39882919e86f70a7b1a5c941
Taylor White <whitetc2@gmail.com>
2023-11-20 15:25:08 -0500

    Target .NET 8

---
+++ src/BlazingApple.Components/BlazingApple.Components/Accordion/AccordionSection.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazingApple.Components.Accordion;
+
+/// <summary>Accordion section</summary>
+public partial class AccordionSection : ComponentBase
+{
+	private bool _isVisible;
+	private readonly Guid id = Guid.NewGuid();
+
+	/// <summary>Accordion parent.</summary>
+	[CascadingParameter]
+	[Parameter]
+	public VerticalAccordion? Parent { get; set; }
+
+	/// <summary>Data to pass to the accordion section.</summary>
+	[Parameter]
+	public (string Heading, string Content)? Data { get; set; }
+}
---
+++ src/BlazingApple.Components/BlazingApple.Components/VerticalAccordion.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazingApple.Components;
+
+/// <summary>Bootstrap styled accordion, vertical layout.</summary>
+public partial class VerticalAccordion : ComponentBase
+{
+	/// <summary>Id in the dom.</summary>
+	public Guid Id { get; set; } = Guid.NewGuid();
+
+	/// <summary>The data to render</summary>
+	[Parameter]
+	public IEnumerable<(string Heading, string Content)>? Items { get; set; }
+}
